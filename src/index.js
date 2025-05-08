d7fd7275f23d6c01e41f77ea1e84fdbea06604cb
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-08 19:26:27 +0530

    Improving reusability of StarRating component with props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color='red'
+        maxRating={10}
+        // With this prop, we will provide the 'StarRating' component
+        // the functionality to update the 'movieRating' state of the 'Test' component.
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} {movieRating !== 1 ? 'stars' : 'star'}</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color='lightgreen' defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
