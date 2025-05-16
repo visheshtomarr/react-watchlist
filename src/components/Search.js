bb86dd9ae7705f660f9c2daaf701adf4666f83e1
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-16 15:35:48 +0530

    Using enter key to search movies

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,30 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        function enterKeyPress(e) {
+            if (document.activeElement === inputEl.current) return;
+
+            if (e.code === "Enter") {
+                inputEl.current.focus();
+                setQuery('');
+            }
+        }
+        document.addEventListener("keydown", enterKeyPress);
+
+        return () => document.addEventListener("keydown", enterKeyPress);
+    }, [setQuery])
+
     return (
         <input
             className="search"
             type="text"
             placeholder="Search movies..."
             value={query}
-            onChange={(e) => setQuery(e.target.value)} />
+            onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
+        />
     );
 }
